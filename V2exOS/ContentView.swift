1182d51be4eb94b38e73753fa9a430d403263987
isaced <isaced@163.com>
2022-07-24 09:22:18 +0800

    Initial Commit

---
+++ V2exOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  V2exOS
+//
+//  Created by isaced on 2022/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ V2exOS/V2exOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  V2exOSApp.swift
+//  V2exOS
+//
+//  Created by isaced on 2022/7/24.
+//
+
+import SwiftUI
+
+@main
+struct V2exOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
