664cff846dfb7b8ff628c4f14d5a4bdea8c10e5e
isaced <isaced@163.com>
2022-07-31 18:56:26 +0800

    RelativeDateTimeFormatter locale

--- V2exOS/Utils/TimeFormat.swift
+++ V2exOS/Utils/TimeFormat.swift
@@ -13,6 +13,7 @@ extension Date {
     // ask for the full relative date
     let formatter = RelativeDateTimeFormatter()
     formatter.unitsStyle = .full
+    formatter.locale = Locale.init(identifier: Locale.preferredLanguages.first!)
 
     // get exampleDate relative to the current date
     let relativeDate = formatter.localizedString(for: self, relativeTo: Date.now)
