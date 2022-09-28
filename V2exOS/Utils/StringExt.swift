4bf52f5614de11fd0c64f45662e0b1b030c19908
isaced <isaced@163.com>
2022-09-28 23:16:12 +0800

    fix InboxListView crash #7

--- V2exOS/Utils/StringExt.swift
+++ V2exOS/Utils/StringExt.swift
@@ -8,9 +8,15 @@
 import Foundation
 
 extension String {
-  func htmlToString() -> String {
-    return  try! NSAttributedString(data: self.data(using: .unicode)!,
-                                    options: [.documentType: NSAttributedString.DocumentType.html],
-                                    documentAttributes: nil).string
+  func htmlToString() -> String? {
+    do {
+      let text = try NSAttributedString(data: self.data(using: .unicode)!,
+                                        options: [.documentType: NSAttributedString.DocumentType.html],
+                                        documentAttributes: nil).string
+      return text
+    } catch {
+      print("htmlToString fail: ", self)
+    }
+    return nil
   }
 }
